fcc567849ac1c60372f5bd87ae02330286ba61ba
Rafsun83 <rafsun7774@gmail.com>
2026-03-01 02:07:30 +0600

    Basic api response added

--- src/main/java/com/example/self_management/controller/WalletController.java
+++ src/main/java/com/example/self_management/controller/WalletController.java
@@ -3,11 +3,13 @@ package com.example.self_management.controller;
 import com.example.self_management.model.domain.Wallet;
 import com.example.self_management.model.dto.wallet.CreateWalletRequest;
 import com.example.self_management.model.dto.wallet.UpdateWalletRequest;
+import com.example.self_management.payload.ApiResponse;
 import com.example.self_management.service.WalletService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,14 +28,18 @@ public class WalletController {
 
     @Operation(summary = "Get All wallet data")
     @GetMapping
-    public List<Wallet> getAllWallet() {
-        return walletService.getAllWallet();
+    public ResponseEntity<ApiResponse<List<Wallet>>>  getAllWallet() {
+        List<Wallet> wallet = walletService.getAllWallet();
+        ApiResponse<List<Wallet>> response = new ApiResponse<>(true, "Wallet fetched successfully!", wallet);
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "Get wallet by Id", description = "Get wallet by Id through the API")
     @GetMapping("{id}")
-    public Wallet getWalletById(@PathVariable Long id) {
-        return walletService.getWalletById(id);
+    public ResponseEntity<ApiResponse<Wallet>> getWalletById(@PathVariable Long id) {
+        Wallet wallet = walletService.getWalletById(id);
+        ApiResponse<Wallet> response = new ApiResponse<>(true, "Wallet fetched successfully!", wallet);
+        return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "Create wallet", description = "Create your wallet")
