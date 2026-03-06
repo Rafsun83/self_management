0950c61177614ce5a2e93b754ad5e5a15db51665
Rafsun83 <rafsun7774@gmail.com>
2026-03-07 02:42:42 +0600

    Implemented update wallet balance

--- src/main/java/com/example/self_management/model/dto/wallet/CreateWalletRequest.java
+++ src/main/java/com/example/self_management/model/dto/wallet/CreateWalletRequest.java
@@ -2,6 +2,7 @@ package com.example.self_management.model.dto.wallet;
 
 
 import com.example.self_management.enums.wallets.WalletType;
+import java.math.BigDecimal;
 
-public record CreateWalletRequest(WalletType type, Number totalAmount, String category, String walletName, String note) {
+public record CreateWalletRequest(WalletType type, BigDecimal totalAmount, String category, String walletName, String note) {
 }
--- src/main/java/com/example/self_management/service/WalletTransactionService.java
+++ src/main/java/com/example/self_management/service/WalletTransactionService.java
@@ -1,5 +1,6 @@
 package com.example.self_management.service;
 
+import com.example.self_management.enums.walletsTransaction.TransactionType;
 import com.example.self_management.mapper.WalletTransactionMapper;
 import com.example.self_management.model.domain.WalletTransaction;
 import com.example.self_management.model.dto.walletTransaction.CreateWalletTransactionRequest;
@@ -7,9 +8,11 @@ import com.example.self_management.persistence.entity.WalletEntity;
 import com.example.self_management.persistence.entity.WalletTransactionEntity;
 import com.example.self_management.persistence.repository.WalletRepository;
 import com.example.self_management.persistence.repository.WalletTransactionRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Service
@@ -29,13 +32,28 @@ public class WalletTransactionService {
         return allTransactionList.stream().map(walletTransactionMapper :: entityToWalletTransactionDomain).toList();
     }
 
+    @Transactional
     public WalletTransaction createWalletTransaction(Long walletId, CreateWalletTransactionRequest createWalletTransactionRequest){
         // 1️⃣ Find wallet
         WalletEntity wallet = walletRepository.findById(walletId)
                 .orElseThrow(() -> new RuntimeException("Wallet not found"));
-
+        //Convert request entity
        var transaction = walletTransactionMapper.createWalletTransactionToEntity(createWalletTransactionRequest);
        transaction.setWalletEntity(wallet);
+
+       //Update wallet balance
+        BigDecimal currentBalance = wallet.getTotalAmount();
+        BigDecimal transactionAmount = transaction.getAmount();
+
+        if(transaction.getType() == TransactionType.DEBIT ){
+            wallet.setTotalAmount(currentBalance.subtract(transactionAmount));
+        }
+        else if(transaction.getType() == TransactionType.CREDIT ){
+            wallet.setTotalAmount(currentBalance.add(transactionAmount));
+        }
+        //Save wallet with updated balance
+        walletRepository.save(wallet);
+
        var saveTransaction  = walletTransactionRepository.save(transaction);
        return walletTransactionMapper.entityToWalletTransactionDomain(saveTransaction);
 
